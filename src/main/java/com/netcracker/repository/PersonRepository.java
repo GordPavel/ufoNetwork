2f0331c125ed4f4d68b8be8bd588c500cc1021a0
teomant <teomant@bk.ru>
2018-03-26 02:58:58 +0400

    User search request

--- src/main/java/com/netcracker/repository/PersonRepository.java
+++ src/main/java/com/netcracker/repository/PersonRepository.java
@@ -4,6 +4,7 @@ import com.netcracker.DAO.GroupEntity;
 import com.netcracker.DAO.PersonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,5 +14,17 @@ import java.util.List;
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
 
     @Query("select pe.groups from PersonEntity pe where pe.id=:id ")
-    List<GroupEntity> getGroups (long Id);
+    List<GroupEntity> getGroups (@Param("id") long Id);
+
+    @Query("select pe from PersonEntity pe where " +
+            "((:name is null )or(?1 IS NOT NULL AND pe.name like ?1)) and " +
+            "((:raceID is null )or(?2 IS NOT NULL AND pe.race.id = ?2 )) and" +
+            "((:ageFrom is null )or(?3 IS NOT NULL AND pe.age >= ?3)) and" +
+            "((:ageTo is null )or(?4 IS NOT NULL AND pe.age <= ?4)) and" +
+            "((:sex is null )or(?5 IS NOT NULL AND pe.sex like ?5))")
+    List<PersonEntity> getBySearchParams(@Param("name") String name,
+                                         @Param("raceID") Integer raceID,
+                                         @Param("ageFrom") Integer ageFrom,
+                                         @Param("ageTo") Integer ageTo,
+                                         @Param("sex") String sex);
 }
